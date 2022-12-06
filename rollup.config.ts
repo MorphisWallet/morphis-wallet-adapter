e28fc169e95ff4329d84f943a9903dfa4377a376
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-06 17:37:15 +0800

    feat(): create development scaffold

---
+++ rollup.config.ts
@@ -0,0 +1,43 @@
+import { resolve } from "path";
+import { defineConfig } from "rollup";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+import { terser } from "rollup-plugin-terser";
+import resolvePlugin from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import cjs2es from "@rollup/plugin-commonjs";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
+
+const config = defineConfig({
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      plugins: [
+        getBabelOutputPlugin({
+          filename: "dist/index.js",
+          configFile: resolve(__dirname, "babel.config.js"),
+        }),
+      ],
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+  ],
+  external: ["@mysten/sui.js"],
+  plugins: [
+    typescript({ target: "es5" }),
+    nodePolyfills(),
+    resolvePlugin({
+      browser: true, // specify that it's built for browser
+    }),
+    cjs2es(),
+  ],
+});
+
+if (process.env.NODE_ENV === "production" && Array.isArray(config.plugins)) {
+  config.plugins.push(terser());
+}
+
+export default config;
